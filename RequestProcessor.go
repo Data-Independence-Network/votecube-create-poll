c73005318e05a2e78e8c415dfff269477565e634
russoturisto <russoturisto@gmail.com>
2019-03-09 17:48:20 -0700

    introduced deserialization context object

--- RequestProcessor.go
+++ RequestProcessor.go
@@ -12,7 +12,7 @@ type RequestProcessor struct {
 	batch *RequestBatch
 }
 
-func (proc *RequestProcessor) startProcessing() {
+func (proc *RequestProcessor) startProcessing(locMaps *deserialize.LocationMaps) {
 
 	for range time.Tick(time.Second * 20) {
 		lastBatch := proc.batch
@@ -21,12 +21,12 @@ func (proc *RequestProcessor) startProcessing() {
 		nextBatch.accept()
 		proc.batch = nextBatch
 
-		proc.processRequest(lastBatch)
+		proc.processRequest(lastBatch, locMaps)
 	}
 
 }
 
-func (proc *RequestProcessor) processRequest(batch *RequestBatch) {
+func (proc *RequestProcessor) processRequest(batch *RequestBatch, locMaps *deserialize.LocationMaps) {
 	time.Sleep(1 * time.Millisecond)
 	close(batch.Add)
 
@@ -34,22 +34,44 @@ func (proc *RequestProcessor) processRequest(batch *RequestBatch) {
 		return
 	}
 
-	for _, request := range batch.Data {
+	idRefs := deserialize.IdReferences{
+		DimDirIdRefs: make(map[int64]map[int]*deserialize.Request),
+		DimIdRefs:    make(map[int64]map[int]*deserialize.Request),
+		DirIdRefs:    make(map[int64]map[int]*deserialize.Request),
+		LabelIdRefs:  make(map[int64]map[int]*deserialize.Request),
+	}
+
+	for index, request := range batch.Data {
 
 		var err error
 		var aPoll models.Poll
 		var cursor int64 = 0
-		ctx := request.ctx
-		data := request.ctx.PostBody()
+		ctx := request.Ctx
+		data := request.Ctx.PostBody()
 		var dataLen = int64(len(data))
 
-		aPoll, err = poll.DeserializePoll(&deserialize.DeserializeContext{
-			&cursor, &data, dataLen, nil, nil, nil, 1,
-		}, err)
+		request.Index = index
+
+		zCtx := deserialize.DeserializeContext{
+			Cursor:  &cursor,
+			Data:    &data,
+			DataLen: dataLen,
+			IdRefs:  &idRefs,
+			LocMaps: locMaps,
+			ReqLocSets: &deserialize.LocationSets{
+				ContinentSet: make(map[int64]bool),
+				CountrySet:   make(map[int64]bool),
+				StateSet:     make(map[int64]bool),
+				TownSet:      make(map[int64]bool),
+			},
+			Request: &request,
+		}
+
+		aPoll, err = poll.DeserializePoll(&zCtx, err)
 
 		if dataLen != cursor {
 			ctx.SetStatusCode(fasthttp.StatusBadRequest)
-			request.done <- true
+			request.Done <- true
 			continue
 		}
 
@@ -57,7 +79,7 @@ func (proc *RequestProcessor) processRequest(batch *RequestBatch) {
 			// then override already written body
 			//ctx.SetBody([]byte("this is completely new body contents"))
 			ctx.SetStatusCode(fasthttp.StatusBadRequest)
-			request.done <- true
+			request.Done <- true
 			continue
 		}
 	}
